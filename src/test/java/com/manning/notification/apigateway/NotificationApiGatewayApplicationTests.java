dac4e483184655a2ea9b96386ef1641ef47b19ef
Mohit Kumar <mkumar@tekion.com>
2021-07-20 19:47:54 +0530

    Initial commit

---
+++ src/test/java/com/manning/notification/apigateway/NotificationApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manning.notification.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
